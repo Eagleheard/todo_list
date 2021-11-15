4cbeaae8e173ee3ca26ecfd0d03f705ce902ebbe
Vladislav Krutikov <Eagleheard5@gmail.com>
2021-11-15 14:51:31 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <div></div>
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [
+        { id: 0, title: 'Create todo app', done: false },
+        { id: 1, title: 'Wash window', done: false },
+        { id: 2, title: 'Go for a walk with smbd', done: false }
+      ]
+    }
+  }
+  render() {
+     const {tasks} = this.state;
+    return (
+      <div className='App'>
+        <h1 className='top'> Active tasks: {tasks.length}</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Task = (props) => {
+ return (
+     <div></div>
+ );
+}
